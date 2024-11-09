c21db2544d0d2661444e90e6cea41db1598c2326
IvanMaltcev <MalthevIU@mail.ru>
2024-11-09 20:04:30 +0300

    initial commit

---
+++ src/main/java/pro/sky/bank_star/BankStarApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.bank_star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStarApplication.class, args);
+	}
+
+}
